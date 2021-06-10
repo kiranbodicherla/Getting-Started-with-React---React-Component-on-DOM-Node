9f392bd9d30215d7f9a4bca77810aeac9625f40e
kiranbodicherla <31443578+kiranbodicherla@users.noreply.github.com>
2021-06-10 22:51:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+  
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+    <App />, document.getElementById("root")
+);
 
